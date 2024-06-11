2c6fd322c594f7af796e1afb601af600cd9e57fc
lavenderYuu <92127470+lavenderYuu@users.noreply.github.com>
2024-06-11 19:06:24 -0400

    Add the backend

---
+++ backend/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.backend;
+
+import com.example.backend.model.Person;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class BackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/main/java/com/example/backend/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Person, Long> {
+}
